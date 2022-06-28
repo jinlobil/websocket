28ecc00c23e4cfdfcb7196a4e9b711ab737c5d37
jinlobil <jinlobil@gmail.com>
2022-06-28 14:30:02 +0900

    stomp 사용을 위한 WebSocketConfig 수정

--- websockettest/src/main/java/com/example/websockettest/config/WebSocketConfig.java
+++ websockettest/src/main/java/com/example/websockettest/config/WebSocketConfig.java
@@ -1,20 +1,26 @@
 package com.example.websockettest.config;
 
-import com.example.websockettest.handler.WebsocketHandler;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
-@RequiredArgsConstructor
+
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-    private final WebsocketHandler websocketHandler;
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(websocketHandler, "/ws/chat").setAllowedOrigins("*");
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/sub");
+        config.setApplicationDestinationPrefixes("/pub");
     }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
+                .withSockJS();
+    }
+
 }
 
