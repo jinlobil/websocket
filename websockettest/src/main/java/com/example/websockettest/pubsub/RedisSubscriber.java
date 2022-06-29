12bb51f4982465ebd1a45832e58ed49993238ea9
jinlobil <jinlobil@gmail.com>
2022-06-29 22:53:33 +0900

    Redis config및 sub 수정

--- websockettest/src/main/java/com/example/websockettest/pubsub/RedisSubscriber.java
+++ websockettest/src/main/java/com/example/websockettest/pubsub/RedisSubscriber.java
@@ -4,35 +4,29 @@ import com.example.websockettest.model.ChatMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
 @Slf4j
-@RequiredArgsConstructor
 @Service
-public class RedisSubscriber implements MessageListener {
+@RequiredArgsConstructor
+public class RedisSubscriber {
 
     private final ObjectMapper objectMapper;
-    private final RedisTemplate redisTemplate;
     private final SimpMessageSendingOperations messagingTemplate;
 
-    /**
-     * Redis에서 메시지가 발행(publish)되면 대기하고 있던 onMessage가 해당 메시지를 받아 처리한다.
-     */
-    @Override
-    public void onMessage(Message message, byte[] pattern) {
+
+    //convertAndSend 로 데이터를 보내면 여기서 잡아서 보낸다.
+    // Redis 에서 메시지가 발행(publish)되면 대기하고 있던 Redis Subscriber 가 해당 메시지를 받아 처리한다.
+    public void sendMessage(String publishMessage) {
+        log.info("데이터가 잘왔나요? publishMessage={}", publishMessage);
         try {
-            // redis에서 발행된 데이터를 받아 deserialize
-            String publishMessage = (String) redisTemplate.getStringSerializer().deserialize(message.getBody());
             // ChatMessage 객채로 맵핑
-            ChatMessage roomMessage = objectMapper.readValue(publishMessage, ChatMessage.class);
-            // Websocket 구독자에게 채팅 메시지 Send
-            messagingTemplate.convertAndSend("/sub/chat/room/" + roomMessage.getRoomId(), roomMessage);
+            ChatMessage chatMessage = objectMapper.readValue(publishMessage, ChatMessage.class);
+            // 채팅방을 구독한 클라이언트에게 메시지 발송
+            messagingTemplate.convertAndSend("/sub/api/chat/rooms/" + chatMessage.getRoomId(), chatMessage);
         } catch (Exception e) {
-            log.error(e.getMessage());
+            log.error("Exception {}", e);
         }
     }
 }
